55bc4c31b883926041bfbc2834e922aecc88c193
kepoorhampond <kepoorh@gmail.com>
2017-03-31 20:46:40 -0700

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # wire-mafia
-:dollar: Block select applications from the wifi. Like the mafia.
+> :dollar: Block select applications from the wifi. Like the mafia.
+
+Currently only working for Linux (possibly OS X), this utility blocks network connections in the specified process or command.
+
+## Demo
+[![asciicast](https://asciinema.org/a/110128.png)](https://asciinema.org/a/110128)
+
+## Install & Usage
+The install is pretty simple, and the only dependency is [`draftlog`](https://github.com/kepoorhampond/python-draftlog).
+```
+$ pip install wire-mafia
+```
+Currently to use it, you can just run `mafia` in your terminal. I plan to add some direct flags so that you don't have to use the CLI.
+
+## Why?
+Originally created because you can't log into the same steam account on multiple computers, so I figured out that you can just turn off your wifi to do it. From there, I still wanted to play pandora and look stuff up, so I made this.
--- wiremafia/blocker.py
+++ wiremafia/blocker.py
@@ -2,20 +2,18 @@ import os, sys
 from .ansi import *
 import subprocess
 
-def get_sudo():
-    # you need sudo access for changing wifi groups
-    try:
-        subprocess.check_call("sudo test .".split(" "))
-    except (subprocess.CalledProcessError, OSError):
-        print (RED + 'Must have sudo access to manage namespaces and network access.' + END)
-        sys.exit(1)
+# you need sudo access for changing wifi groups
+try:
+    subprocess.check_call("sudo test .".split(" "))
+except (subprocess.CalledProcessError, OSError):
+    print (RED + 'Must have sudo access to manage namespaces and network access.' + END)
+    sys.exit(1)
 
-    # set up the namespace "python-blockade" with no network access
-    os.system("sudo ip netns add python-blockade > /dev/null 2>&1")
+# set up the namespace "python-blockade" with no network access
+os.system("sudo ip netns add python-blockade > /dev/null 2>&1")
 
 
 def block_program(program, pid=None):
-    get_sudo()
     # Get the current user
     user = os.popen("whoami").read().strip("\n")
 
